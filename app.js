2c9542e9f53e44e95e3471cc60a6809e9e3313d4
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-02-10 01:16:52 +0000

    update

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
-if (chute == numeroSecreto){
-    alert(`Parabéns, você acertou o número secreto (${numeroSecreto})`);
-} else {
-    alert('Você errou :(');
-}
+let numeroSecreto = 77;
+let chute;
+let tentativas = 1;
+
+//enquanto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Parabéns, você acertou o número secreto (${numeroSecreto}) com ${tentativas} tentativas!`);
+        }else {
+            if (chute > numeroSecreto) {
+                alert(`O número secreto é menor que ${chute}`);
+            } 
+            else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
+            // tentativas = tentativas +1;
+            tentativas++;
+        }
+    }
+
+
+
+
